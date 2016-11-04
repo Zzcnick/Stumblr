bd800c1f69a4e99be1034d4e0db76d3206057779
Mikhail Kotlik <mkotlik@stuy.edu>
2016-11-04 14:51:18 -0400

    Outlined auth.py

---
+++ utils/auth.py
@@ -0,0 +1,35 @@
+#Stumblr - auth.py
+#Authentication Module
+#Written by Misha
+
+import sqlite3, hashlib
+
+#Login Function
+#boolean login(username [str], password [str])
+def login(username, password):
+
+#Register Function
+#boolean register(username [str], password [str])
+def register(username, password):
+
+#Verify_Clean Helper Function
+#boolean verify_clean(username [str], password [str])
+def verify_clean(username, password):
+
+
+#=====CONNECTION FUNCTIONS=====#
+
+#Connection connect()
+def connect():
+    #Place-holder return
+    return True
+
+#void disconnect(db [Connection])
+def disconnect(db):
+    #Place-holder return
+    return True
+
+#boolean init(db [Connection])
+def init(db):
+    #Place-holder return
+    return True
