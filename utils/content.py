0fdc7a4109b379278118173fa92344cce33146c2
Zzcnick <zzhen@stuy.edu>
2016-11-06 13:50:16 -0500

    restructure content.py, add add_story and documentation

--- utils/content.py
+++ utils/content.py
@@ -3,19 +3,32 @@ import csv
 
 # DOCUMENTATION
 # ==========================================================================
-# cursor init()
-# precond: 
-# postcond: connects to database
+# Database Schema:
+#     userdata      | username TEXT | password TEXT | email    TEXT |
+#     story_content | username TEXT | storyID  INT  | content  TEXT | sequence INT  |
+#     story_id      | storyID  INT  | title    TEXT |
 #
-# int insert_user (String username, String password, String email)
+# database connect()
+# precond: database is not open
+# postcond: connects to database and returns the database object
+#
+# void disconnect()
+# precond: database is open
+# postcond: takes a database object, saves it, and closes it
+#
+# void init() 
+# precond:
+# postcond: creates tables in database if they don't already exist
+#
+# int add_user (String username, String password, String email)
 # precond: inputs are sanitized
 # postcond: inserts the appropriate data into the userdata database
 #           returns  0 if successful
 #                   -1 if unsuccessful
 #
-# int insert_story (String username, String content)
+# int add_story (String username, String title, String content)
 # precond: inputs are sanitized, content is less than 200 chars
-# postcond: inserts the appopriate data in the story_content database
+# postcond: inserts the appropriate data in the story_content database
 #           returns  0 if successful
 #                   -1 if unsuccessful
 #
@@ -26,26 +39,29 @@ import csv
 
 # Table Editing Functions
 # ==========================================================================
-# Creating Database
 def connect():
     name = "./data/unencryptedpasswords.db"
     db = sqlite3.connect(name)
     c = db.cursor()
-
-    # Creating Tables
-    cmd = "CREATE TABLE IF NOT EXISTS userdata (username TEXT, password TEXT, email TEXT)"
-    c.execute(cmd);
-    cmd = "CREATE TABLE IF NOT EXISTS story_content (username TEXT, storyID INTEGER, content TEXT, sequence INTEGER)"
-    c.execute(cmd);
-    cmd = "CREATE TABLE IF NOT EXISTS story_id (storyID INTEGER, title TEXT)"
-    c.execute(cmd);
     return db
 
 def disconnect(db):
     db.commit()
     db.close()
+
+def init():
+    db = connect()
+    c = db.cursor()
+    # Creating Tables
+    cmd = "CREATE TABLE IF NOT EXISTS userdata (username TEXT, password TEXT, email TEXT)"
+    c.execute(cmd)
+    cmd = "CREATE TABLE IF NOT EXISTS story_content (username TEXT, storyID INTEGER, content TEXT, sequence INTEGER)"
+    c.execute(cmd)
+    cmd = "CREATE TABLE IF NOT EXISTS story_id (storyID INTEGER, title TEXT)"
+    c.execute(cmd)
+    disconnect(db)
     
-def insert_user(username, password, email):
+def add_user(username, password, email):
     try:
         db = connect()
         c = db.cursor()
@@ -56,21 +72,48 @@ def insert_user(username, password, email):
     except:
         return "-1"
 
-def insert_story(username, content):
+def add_story(username, title, content):
     try:
         db = connect()
         c = db.cursor()
-        sid = get_last_story + 1
-        seq = get_sequence(sid) + 1
+        sid = largest_sid() + 1
+        seq = largest_sequence(sid) + 1
         req = "INSERT INTO story_content VALUES ('%s', %s, '%s', %s)"%(username,sid,content,seq)
         c.execute(req)
-        db.close()
+        req = "INSERT INTO story_id VALUES (%s, '%s')"%(sid,title)
+        c.execute(req)
+        disconnect(db)
         return "0"
     except:
         return "-1"
 
 # Table Accessing Functions
 # ==========================================================================
-def get_last_story():
+def largest_sid():
+    db = connect()
+    c = db.cursor()
     req = "SELECT storyID FROM story_id"
-    
+    data = c.execute(req)
+    maxSID = -1
+    for entry in data: 
+        if entry[0] > maxSID:
+            maxSID = entry[0]
+    disconnect(db)
+    return maxSID
+
+def largest_sequence(sid):
+    db = connect()
+    c = db.cursor()
+    req = "SELECT sequence FROM story_content WHERE storyID == %s"%(sid)
+    data = c.execute(req)
+    maxSEQ = -1
+    for entry in data: 
+        if entry[0] > maxSEQ:
+            maxSEQ = entry[0]
+    disconnect(db)
+    return maxSEQ
+
+# Initialization
+# ==========================================================================
+if (__name__ == "__main__"):
+    init()
