48e3117f5cc2c2783a3d9a2087599476baa68645
yawata159 <seijiyawata@gmail.com>
2016-11-06 16:28:56 -0500

    Templates

--- testing.py
+++ testing.py
@@ -5,16 +5,21 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     #if not logged in:
-    # return render_template("home.html")
+    return render_template("home.html")
     #if logged in:
-    '''
-    there should be two options:contributed to and not contributed to
-    pass two data struct w/ contrib, and non-contrib stories
-    contrib should have full text,
-    non-contrib should have latest addition
-    '''
-    return render_template("stories.html")
+    
+    ###there should be two options:contributed to and not contributed to
+    ###pass two data struct w/ contrib, and non-contrib stories
+    ###contrib should have full text,
+    ###non-contrib should have latest addition
+    #return render_template("stories.html")
 
+    
+#fort testing purposes
+@app.route("/stories/")
+def stories():
+    return render_template("stories.html")
+    
 @app.route("/login/")
 def login():
     #if logged in:
@@ -40,13 +45,11 @@ def story(sid):
     #if not logged in:
     #return redirect(url_for("login"))
     #if logged in:
-    '''
-    if it has been contributed to:
-    pass a data struct w/ title & whole story
-    if not:
-    pass a data struct w/ title & latest addition
-    or some other way idk
-    '''
+    ###if it has been contributed to:
+    ###pass a data struct w/ title & whole story
+    ###if not:
+    ###pass a data struct w/ title & latest addition
+    ###or some other way idk
     return render_template("story.html")
 
 if __name__ == '__main__':
